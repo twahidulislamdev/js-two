235180679a773982369fcea8d8e28943b5300953
Twahidul Islam <twahidulislam2005@gmail.com>
2026-01-05 12:00:00 +0600

    Add Some thing

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ let gender = "Male";
 let age = 25;
 let area = "Mohakhali";
 let salary = 155000;
+
+if (gender == "Male") {
+  if (age <= 30 && age >= 21) {
+    if (salary >= 100000 || area == "Dhaka") {
+      console.log("Yes!!!! Candidate Matched");
+    } else {
+      console.log("we are looking for better");
+    }
+  } else {
+    console.log("Age does not match our Requirement");
+  }
+} else {
+  console.log("We are looking for 'Female' candidate");
+}
