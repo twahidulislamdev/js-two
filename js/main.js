6814b5931ca6eefc24a5e95c05c9efcd83ed172f
Twahidul Islam <twahidulislam2005@gmail.com>
2026-01-06 12:00:00 +0600

    Add Some thing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // if else statement
-let age = 20;
-if (age >= 21) {
-  console.log("You Can Marry");
-}
-else {
-  console.log("You Are not able to get marry");
-}
+// let age = 20;
+// if (age >= 21) {
+//   console.log("You Can Marry");
+// } else {
+//   console.log("You Are not able to get marry");
+// }
+
+// Metrimonial Site (Nested If else)
+let gender = "Male";
+let age = 25;
+let area = "Mohakhali";
+let salary = 155000;
